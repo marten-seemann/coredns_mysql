bc17967ad34bfc8767dbb005fca17674fa283bc3
Marten Seemann <martenseemann@gmail.com>
2022-03-12 15:09:47 +0400

    return the correct SOA record when no records are found

--- handler.go
+++ handler.go
@@ -51,12 +51,11 @@ func (handler *CoreDNSMySql) ServeDNS(ctx context.Context, w dns.ResponseWriter,
 
 	if len(records) == 0 {
 		// no record found but we are going to return a SOA
-		rec := &Record{
-			Name:       qName,
-			RecordType: "SOA",
-			Content:    "{}",
+		recs, err := handler.findRecord(qZone, "", "SOA")
+		if err != nil {
+			return handler.errorResponse(state, dns.RcodeServerFailure, err)
 		}
-		records = append(records, rec)
+		records = append(records, recs...)
 	}
 
 	if qType == "AXFR" {
